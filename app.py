80be82fcfcabb835b875687fb445c78e8df3f9ac
Matthew <58238450+Weezity@users.noreply.github.com>
2020-08-15 14:51:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,4 @@ def deploy(search1,search2):
         return render_template("results.html",compare=compare,)
     
 
-if __name__ == "_)main__":
-    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0')
+
