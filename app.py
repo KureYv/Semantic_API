898f368728aee7db05fe4724d34e2c8591439685
Matthew <58238450+Weezity@users.noreply.github.com>
2020-08-14 20:44:40 -0400

    Threading update

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, url_for, make_response,jsonify,request
 import tensorflow_hub as hub
 import numpy as np
+import threading
 
 
 app = Flask(__name__)
 
-module_url = "https://tfhub.dev/google/universal-sentence-encoder/4" #@param ["https://tfhub.dev/google/universal-sentence-encoder/4", "https://tfhub.dev/google/universal-sentence-encoder-large/5"]
-model = hub.load(module_url)
 
 
 
@@ -14,8 +13,14 @@ def semantic(search1,search2):
     comparison = model([search1,search2])
     return np.inner(comparison[0],comparison[1])
 
+def task():
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder/4" #@param ["https://tfhub.dev/google/universal-sentence-encoder/4", "https://tfhub.dev/google/universal-sentence-encoder-large/5"]
+    model = hub.load(module_url)
+
+
 @app.route('/')
 def menu():
+    threading.Thread(target=task).start()
     return render_template("index.html")
 
 @app.route('/<search1>/<search2>',methods=['POST','GET'])
